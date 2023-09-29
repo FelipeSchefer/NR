9302d4ba20e29e5d71d12ec7cbcdf7f779c0e912
felipe Teixeira Schefer <felipe_official@outlook.com>
2023-09-28 21:49:21 -0300

    back-end

---
+++ back-end/src/server.ts
@@ -0,0 +1,26 @@
+import express, {Request, Response} from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+
+const path = require('path')
+// const adminRouter = require('./routes/admin')
+// const shopRouter = require('./routes/shop')
+
+const app = express()     
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+// app.use(adminRouter)
+// app.use(shopRouter)
+
+app.use((req: Request, res: Response)=>{
+  res.status(200).send('Bem-vindo ao servidor Node.js com TypeScript!');
+})
+
+app.use((req: Request, res: Response)=>{
+ res.status(401).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+const server = http.createServer(app)
+server.listen('3001', (): void =>{
+ console.log("Server running!")
+})
